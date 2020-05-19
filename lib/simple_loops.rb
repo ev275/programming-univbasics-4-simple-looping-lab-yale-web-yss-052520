0b72f8d162c85f99017d95b2cb6f22c2e3be472b
Enrique Valencia <enrique.valencia@yale.edu>
2020-05-19 02:35:45 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,3 +21,12 @@ def output_array(array)
   end
 end
 
+def return_string_array(array)
+  count = 0
+  while array[count] do
+    array[count] = array[count].to_s
+    count += 1
+  end
+  array
+end
+
